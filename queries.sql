a62278e059628bee7c397c0d9da44369590018b0
DoubleM85 <matt.mason303@gmail.com>
2024-04-25 20:36:53 -0600

    almost done with 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,44 @@
+--- #4 pull a clean trips file
+
+SELECT *
+FROM trips
+WHERE tripduration > 1 AND tripduration < (60*24)
+
+
+------------------------------------------------
+WITH s AS
+	(SELECT
+		DISTINCT(pubdatetime::date) as date,
+	 	companyname AS company,
+		sumdid,
+		MIN(chargelevel)
+	FROM scooters
+	WHERE chargelevel > 0
+	GROUP BY date, company, sumdid),
+
+t AS
+	(SELECT
+		DISTINCT(pubtimestamp::date) as date,
+		companyname AS company,
+		triprecordnum as trip_id,
+		sumdid
+	FROM trips
+	WHERE tripduration > 1 AND tripduration < (60*24))
+	
 SELECT
-	sumdid,
-	pubdatetime::date,
-	chargelevel
-FROM scooters
-WHERE chargelevel > 0
-ORDER BY chargelevel ASC
+	s.date,
+	s.company,
+	s.sumdid,
+	trip_id
+FROM s
+LEFT JOIN t
+USING (sumdid, date)
+
+		
+SELECT DISTINCT(companyname) AS company_name,
+	startdate AS start_date,
+	EXTRACT(HOUR from starttime) AS hour,
+	COUNT(DISTINCT triprecordnum) AS scooters_needed
+FROM trips
+GROUP BY company_name, start_date, hour
+LIMIT 5;
